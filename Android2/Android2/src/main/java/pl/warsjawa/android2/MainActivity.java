83d09fe615b63d00381400f4bf1a376216515323
Maciej Górski <maciek.gorski@gmail.com>
2013-09-25 00:34:22 +0200

    added layout with DrawerLayout for MainActivity

--- Android2/Android2/src/main/java/pl/warsjawa/android2/MainActivity.java
+++ Android2/Android2/src/main/java/pl/warsjawa/android2/MainActivity.java
@@ -1,4 +1,14 @@
 package pl.warsjawa.android2;
 
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+
 public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        DrawerLayout drawerLayout = findView(R.id.main_layout);
+    }
 }
