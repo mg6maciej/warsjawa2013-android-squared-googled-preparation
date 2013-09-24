f861bf98576eb5ada1ddd19ef2d68c013e92b40a
Maciej Górski <maciek.gorski@gmail.com>
2013-09-24 17:48:43 +0200

    added Android^2 project, woohoo!

---
+++ Android2/Android2/src/main/java/pl/warsjawa/android2/LauncherActivity.java
@@ -0,0 +1,14 @@
+package pl.warsjawa.android2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class LauncherActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
