5d55707eeaa7bff52a01ed8f8966e71e96ba9eda
Maciej Górski <maciek.gorski@gmail.com>
2013-09-25 13:58:10 +0200

    added simple map display

--- Android2/Android2/src/main/java/pl/warsjawa/android2/MeetupsMapFragment.java
+++ Android2/Android2/src/main/java/pl/warsjawa/android2/MeetupsMapFragment.java
@@ -2,9 +2,13 @@ package pl.warsjawa.android2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MeetupsMapFragment extends Fragment {
 
@@ -16,6 +20,13 @@ public class MeetupsMapFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        
+        FragmentManager fm = getChildFragmentManager();
+        Fragment f = fm.findFragmentById(R.id.meetups_map_container);
+        if (f == null) {
+            f = new SupportMapFragment();
+            FragmentTransaction tx = fm.beginTransaction();
+            tx.add(R.id.meetups_map_container, f);
+            tx.commit();
+        }
     }
 }
