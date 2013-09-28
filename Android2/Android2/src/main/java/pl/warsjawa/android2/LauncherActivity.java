5bfdbcce0b76056adbf86a5642391d44d2b42433
Maciej Górski <maciek.gorski@gmail.com>
2013-09-29 01:12:29 +0200

    WebView instead of starting external browser to log on

--- Android2/Android2/src/main/java/pl/warsjawa/android2/LauncherActivity.java
+++ Android2/Android2/src/main/java/pl/warsjawa/android2/LauncherActivity.java
@@ -9,12 +9,7 @@ public class LauncherActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("https://secure.meetup.com/oauth2/authorize"
-                + "?client_id=a7r6qufa5utub7l5m1eva4hsaa"
-                + "&response_type=token"
-                + "&redirect_uri=warsjawa://android2"
-                + "&set_mobile=on"));
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
